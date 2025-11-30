45b1f517092eafa5084125642ef688acab1c1e79
yari :3 <barkbarkbark@yari.dog>
2025-11-30 17:36:39 +0000

    fixed some qute shit

--- config/qutebrowser/default.nix
+++ config/qutebrowser/default.nix
@@ -2,4 +2,6 @@
 
 {
   home.packages = with pkgs; [ qutebrowser ];
+  home.file.".config/qutebrowser/config.py".source = ./config/config.py;
+  home.file.".config/qutebrowser/custom".source = ./config/custom;
 }
