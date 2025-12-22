c960ad07faa70bb1d5cd723de95db9a6e9fe1b9c
yari :3 <barkbarkbark@yari.dog>
2025-12-22 12:13:21 +0000

    swayidle from list to attrset

--- config/niri/default.nix
+++ config/niri/default.nix
@@ -24,12 +24,6 @@
     let
       # Lock command
       lock = "${pkgs.swaylock}/bin/swaylock --daemonize";
-      # TODO: modify "display" function based on your window manager
-      # Sway
-      # display = status: "${pkgs.sway}/bin/swaymsg 'output * power ${status}'";
-      # Hyprland
-      # display = status: "hyprctl dispatch dpms ${status}";
-      # Niri
       display = status: "${pkgs.niri}/bin/niri msg action power-${status}-monitors";
     in
     {
@@ -53,24 +47,11 @@
           command = "${pkgs.systemd}/bin/systemctl suspend";
         }
       ];
-      events = [
-        {
-          event = "before-sleep";
-          # adding duplicated entries for the same event may not work
-          command = (display "off") + "; " + lock;
-        }
-        {
-          event = "after-resume";
-          command = display "on";
-        }
-        {
-          event = "lock";
-          command = (display "off") + "; " + lock;
-        }
-        {
-          event = "unlock";
-          command = display "on";
-        }
-      ];
+      events = {
+        before-sleep = (display "off") + "; " + lock;
+        after-resume = display "on";
+        lock = (display "off") + "; " + lock;
+        unlock = display "on";
+      };
     };
 }
