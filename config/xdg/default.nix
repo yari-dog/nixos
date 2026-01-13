42728bb2352265e6a0056be5581b653d640ca28c
yari :3 <barkbarkbark@yari.dog>
2026-01-13 16:19:58 +0000

    flake.lock

--- config/xdg/default.nix
+++ config/xdg/default.nix
@@ -1,32 +1,31 @@
 { pkgs, ... }:
 
 {
-  # xdg.portal = {
-  #   enable = true;
-  #   extraPortals = with pkgs; [
-  #     xdg-desktop-portal-gnome
-  #     xdg-desktop-portal-gtk
-  #     # xdg-desktop-portal-termfilechooser
-  #   ];
-  #   xdgOpenUsePortal = true;
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk
+      # xdg-desktop-portal-termfilechooser
+    ];
+    xdgOpenUsePortal = true;
+
+    config.common = {
+      "default" = "gtk";
+      "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+    };
+  };
+  xdg.terminal-exec = {
+    enable = true;
+    settings = {
+      default = [
+        "foot.desktop"
+      ];
+    };
+  };
   #
-  #   config.common = {
-  #     "default" = "gtk";
-  #     "org.freedesktop.impl.portal.ScreenCast" = "gnome";
-  #     "org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
-  #   };
-  # };
-  # xdg.terminal-exec = {
-  #   enable = true;
-  #   settings = {
-  #     default = [
-  #       "foot.desktop"
-  #     ];
-  #   };
-  # };
-  #
-  # home.sessionVariables.TERMCMD = "foot";
-  # home.sessionVariables.TERMINAL = "foot";
+  home.sessionVariables.TERMCMD = "foot";
+  home.sessionVariables.TERMINAL = "foot";
   home.sessionVariables.EDITOR = "nvim";
 
   # xdg.configFile."xdg-desktop-portal-termfilechooser/config" = {
