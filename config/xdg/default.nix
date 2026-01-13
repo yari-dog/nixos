cac4be10bb84d128b0e4e3f063d71fb7ca03177e
yari :3 <barkbarkbark@yari.dog>
2026-01-13 14:06:53 +0000

    fix some xdg bullshit

--- config/xdg/default.nix
+++ config/xdg/default.nix
@@ -1,40 +1,40 @@
 { pkgs, ... }:
 
 {
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gnome
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-termfilechooser
-    ];
-    xdgOpenUsePortal = true;
-
-    config.common = {
-      "default" = "gtk";
-      "org.freedesktop.impl.portal.ScreenCast" = "gnome";
-      "org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
-    };
-  };
-  xdg.terminal-exec = {
-    enable = true;
-    settings = {
-      default = [
-        "foot.desktop"
-      ];
-    };
-  };
-
-  home.sessionVariables.TERMCMD = "foot";
-  home.sessionVariables.TERMINAL = "foot";
+  # xdg.portal = {
+  #   enable = true;
+  #   extraPortals = with pkgs; [
+  #     xdg-desktop-portal-gnome
+  #     xdg-desktop-portal-gtk
+  #     # xdg-desktop-portal-termfilechooser
+  #   ];
+  #   xdgOpenUsePortal = true;
+  #
+  #   config.common = {
+  #     "default" = "gtk";
+  #     "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+  #     "org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
+  #   };
+  # };
+  # xdg.terminal-exec = {
+  #   enable = true;
+  #   settings = {
+  #     default = [
+  #       "foot.desktop"
+  #     ];
+  #   };
+  # };
+  #
+  # home.sessionVariables.TERMCMD = "foot";
+  # home.sessionVariables.TERMINAL = "foot";
   home.sessionVariables.EDITOR = "nvim";
 
-  xdg.configFile."xdg-desktop-portal-termfilechooser/config" = {
-    force = true;
-    text = ''
-      [filechooser]
-      cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
-    '';
-  };
+  # xdg.configFile."xdg-desktop-portal-termfilechooser/config" = {
+  #   force = true;
+  #   text = ''
+  #     [filechooser]
+  #     cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
+  #   '';
+  # };
 
 }
