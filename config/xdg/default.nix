ed9b107a61ea0df11b389308f9c4e05cb69dbc0c
yari :3 <yari_dog@protonmail.com>
2026-01-26 12:14:05 +0000

    yur

--- config/xdg/default.nix
+++ config/xdg/default.nix
@@ -27,4 +27,5 @@
   home.sessionVariables.TERMCMD = "foot";
   home.sessionVariables.TERMINAL = "foot";
   home.sessionVariables.EDITOR = "nvim";
+  home.sessionVariables.NIXPKGS_ALLOW_UNFREE = "1";
 }
