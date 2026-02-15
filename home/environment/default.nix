98989094ac1b3b86b22ea2646ee5d2bed20ccabd
yari :3 <yari@woofwoofwoof.net>
2026-02-15 20:09:30 +0000

    maybe fixed rofi terminal problem

--- home/environment/default.nix
+++ home/environment/default.nix
@@ -1,9 +1,6 @@
 { pkgs, lib, ... }:
 
 {
-  home.sessionVariables.TERM = "foot";
-  home.sessionVariables.TERMCMD = "foot";
-  home.sessionVariables.TERMINAL = "foot";
   home.sessionVariables.EDITOR = "nvim";
   home.sessionVariables.NIXPKGS_ALLOW_UNFREE = "1";
 }
