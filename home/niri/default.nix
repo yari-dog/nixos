cd4df1c66ead63ce32e1745f513fc0d64c91e445
yari :3 <yari@woofwoofwoof.net>
2026-02-21 12:36:58 +0000

    added gnome polkit agent to niri conf

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -55,4 +55,21 @@
         unlock = display "on";
       };
     };
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    Unit = {
+      Description = "polkit-gnome-authentication-agent-1";
+      Wants = [ "graphical-session.target" ];
+      After = [ "graphical-session.target" ];
+    };
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
 }
