74a6c11c83b53a7d0770e759bde59b943fb38650
yari :3 <yari@woofwoofwoof.net>
2026-03-01 14:53:44 +0000

    fixing some annoyances with nvim

--- home/nvim/plugins/conform.nix
+++ home/nvim/plugins/conform.nix
@@ -15,7 +15,7 @@
           -- Disable "format_on_save lsp_fallback" for lanuages that don't
           -- have a well standardized coding style. You can add additional
           -- lanuages here or re-enable it for the disabled ones.
-          local disable_filetypes = { c = true, cpp = true }
+          local disable_filetypes = { }
           if disable_filetypes[vim.bo[bufnr].filetype] then
             return nil
           else
--- home/nvim/plugins/treesitter.nix
+++ home/nvim/plugins/treesitter.nix
@@ -7,64 +7,7 @@
 
     # Installing tree-sitter grammars from Nixpkgs (recommended)
     # https://nix-community.github.io/nixvim/plugins/treesitter/index.html#installing-tree-sitter-grammars-from-nixpkgs
-    # grammarPackages = pkgs.vimPlugins.nvim-treesitter.passthru.allGrammars;
-    grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-      # Linux
-      bash
-      ssh_config
-      # sway
-      tmux
-
-      # Nix, Nixvim
-      nix
-      query # treesitter queries
-      vim
-      vimdoc
-      # lua
-      # luadoc
-
-      # General Development
-      csv
-      diff
-      editorconfig
-      git_config
-      git_rebase
-      gitattributes
-      gitcommit
-      gitignore
-      ini
-      # llvm
-      markdown
-      markdown_inline
-      regex
-      # xml
-      yaml
-
-      # Rust Development
-      rust
-      toml # Also for ZMK `keymap.toml`
-
-      # Web Development
-      css
-      html
-      # http
-      javascript
-      json
-      # json5
-      # php
-      # php_only
-      # phpdoc
-      # sql
-      # scss
-      # twig
-      # tsx
-      # typescript
-
-      # Web - other
-      # astro
-      # nginx
-      # svelte
-    ];
+    grammarPackages = pkgs.vimPlugins.nvim-treesitter.passthru.allGrammars;
 
     settings = {
       # Installing tree-sitter grammars from nvim-treesitter
@@ -82,12 +25,8 @@
         ];
       };
 
-      indent = {
-        enable = true;
-        disable = [
-          "ruby"
-        ];
-      };
+      indent.enable = true;
+      folding.enable = true;
 
       # There are additional nvim-treesitter modules that you can use to interact
       # with nvim-treesitter. You should go explore a few and see what interests you:
@@ -97,4 +36,5 @@
       #    - Treesitter + textobjects: https://nix-community.github.io/nixvim/plugins/treesitter-textobjects/index.html
     };
   };
+  plugins.treesitter-context.enable = true;
 }
