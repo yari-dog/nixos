f4d0ef2e763f2a1d8ac7700c513a7ac2a3d985b3
yari :3 <yari@woofwoofwoof.net>
2026-03-29 16:09:54 +0100

    remove old opencl stuff?

--- hosts/yiff/default.nix
+++ hosts/yiff/default.nix
@@ -12,15 +12,15 @@
   boot.kernelParams = [
     "video=DP-1:3440x1440@75.050"
   ];
-  environment.variables = {
-    RUSTICL_ENABLE = "radeonsi";
-  };
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      mesa.opencl # Enables Rusticl (OpenCL) support
-    ];
-  };
+  # environment.variables = {
+  #   RUSTICL_ENABLE = "radeonsi";
+  # };
+  # hardware.graphics = {
+  #   enable = true;
+  #   extraPackages = with pkgs; [
+  #     mesa.opencl # Enables Rusticl (OpenCL) support
+  #   ];
+  # };
   hardware.amdgpu.opencl.enable = true;
 
   # Configure keymap in X11
