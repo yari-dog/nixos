0473fe3731a6ffbea026b33a8fe1f344c0752092
yari :3 <yari@woofwoofwoof.net>
2026-06-27 11:57:33 +0100

    mime types still fucked

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -1,5 +1,27 @@
 { pkgs, lib, ... }:
 {
+  xdg.mime = {
+    enable = true;
+    defaultApplications = {
+      "image/*" = [ "feh.desktop" ];
+      "video/*" = [ "mpv.desktop" ];
+      "x-scheme-handler/http" = [ "zen.desktop" ];
+      "x-scheme-handler/https" = [ "zen.desktop" ];
+      # if steam links arent opening my current fix is that x-scheme-handler/steam needs to be in .local/share/applications/mimelist.cache for some reason. no fucking idea why
+    };
+    removedAssociations = {
+      "video/*" = [ "umpv.desktop" ];
+      "audio/*" = [ "umpv.desktop" ];
+      "image/*" = [
+        "org.qutebrowser.qutebrowser.desktop"
+        "helium.desktop"
+        "gimp.desktop"
+        "org.darktable.darktable.desktop"
+      ];
+      "application/zip" = [ "org.prismlauncher.PrismLauncher.desktop" ];
+    };
+  };
+
   xdg.portal = {
     enable = true;
     wlr.enable = true;
